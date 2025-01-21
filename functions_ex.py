3b7a5e5f0d50f24c4032298463bd1825009190d7
ragh31ragh <ragh31ragh@gmail.com>
2025-01-21 18:50:00 +0530

    latest changes

--- functions_ex.py
+++ functions_ex.py
@@ -2,11 +2,11 @@
 to_seconds = 24 * 60 * 60;
 name_of_unit = "seconds";
 
-def days_to_units(num_of_days):
+def days_to_units(num_of_days,custom_message):
     print(f"{num_of_days} days are {num_of_days * to_seconds} {name_of_unit}");
     print("All good");
 
-days_to_units(35)
-days_to_units(20)
-days_to_units(50)
-days_to_units(110)
+days_to_units(35,"This is 1")
+days_to_units(20,"Good")
+days_to_units(50,"Third")
+days_to_units(110,"very good")
