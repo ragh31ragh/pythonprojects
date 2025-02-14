4c3f42f17b7128a36191695b9df072033a9be0f6
ragh31ragh <ragh31ragh@gmail.com>
2025-02-14 10:46:35 +0530

    changes in while loop

--- common_files/built_in_functions_ex.py
+++ common_files/built_in_functions_ex.py
@@ -1,7 +1,9 @@
 print("hello world")
 input("enter number")
-set ([3,4,5,5,])
+set =([3,4,5,5,])
 int("4")
 
 "2,3".split(",")
 "test".split("e")
+
+print(set)
--- common_files/whileloop.py
+++ common_files/whileloop.py
@@ -1,4 +1,13 @@
 i = 1
+j = 10
+
+
 while i < 10 :
-    print (i*'*')
+    new= i*2
+    print (new *'*')
     i = i + 1
+
+while j >= 1 :
+    new= j*2
+    print (new *'*')
+    j = j - 1
